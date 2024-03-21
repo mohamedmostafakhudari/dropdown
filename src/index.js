c0091fc49a3d7a0c4e2a67f77b5c3705594915cb
Mohamed Mostafa <mohamedmostafakhudari@gmail.com>
2024-03-21 16:50:28 +0200

    Create a dropdown ready to use

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const dropdownButton = document.querySelector(".dropdown__button");
+
+dropdownButton.addEventListener("click", () => {
+  const dropdown = dropdownButton.parentElement;
+
+  if (dropdown.getAttribute("data-state") === "idle") {
+    dropdown.setAttribute("data-state", "active");
+  } else if (dropdown.getAttribute("data-state") === "active") {
+    dropdown.setAttribute("data-state", "idle");
+  }
+});
--- webpack.common.js
+++ webpack.common.js
@@ -2,32 +2,32 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.js",
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Template 2",
-			template: "./src/template.html",
-		}),
-	],
-	output: {
-		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader", "postcss-loader"],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: "asset/resource",
-			},
-		],
-	},
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Dropdown",
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
